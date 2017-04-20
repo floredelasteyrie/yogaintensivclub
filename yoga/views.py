1523d3e8a728272e72d3a83e0ea1e55a0c31547d
FloLast <flore.delasteyrie@gmail.com>
2017-04-20 17:14:16 +0200

    MAJ 20/04
    - Ajout d'un premier leaderboard

--- yoga/views.py
+++ yoga/views.py
@@ -73,23 +73,67 @@ def leaderboard(request):
     if not request.user.is_authenticated:
         return redirect("/login")
 
-    userlist = User.objects.all()
-    sportlist = Activity.objects.all()
-    tableau = []
-    print(len(userlist))
-    compteur = 0
-    for sport in sportlist:
-        for utilisateur in userlist:
-            activitylist = Activity.objects.filter(user=utilisateur,sport=sport)
-            temps = 0
-            for activitée in activitylist:
-                temps += activitée.durée
-            trinome = {'temps': temps, 'utilisateur': utilisateur, 'sport': sport }
-            print(trinome)
-            print(compteur)
-            tableau.append(trinome)
-            compteur += 1
+    userlist1 = User.objects.all()
+    tableau1 = []
+    compteur1 = 1
+    for utilisateur in userlist1:
+        activitylist1 = Activity.objects.filter(user=utilisateur,sport="Yoga")
+        temps = 0
+        for activitée in activitylist1:
+            temps += activitée.durée
+            print(temps)
+        binome1 = {'temps': temps, 'utilisateur': utilisateur}
+        tableau1.append(binome1)
+        compteur1 += 1
 
+    userlist2 = User.objects.all()
+    tableau2 = []
+    compteur2 = 1
+    for utilisateur in userlist2:
+        activitylist2 = Activity.objects.filter(user=utilisateur,sport="Top Body Challenge")
+        temps = 0
+        for activitée in activitylist2:
+            temps += activitée.durée
+        binome2 = {'temps': temps, 'utilisateur': utilisateur}
+        tableau2.append(binome2)
+        compteur2 += 1
+
+    userlist3 = User.objects.all()
+    tableau3 = []
+    compteur3 = 1
+    for utilisateur in userlist3:
+        activitylist3 = Activity.objects.filter(user=utilisateur,sport="Cours les Mills")
+        temps = 0
+        for activitée in activitylist3:
+            temps += activitée.durée
+        binome3 = {'temps': temps, 'utilisateur': utilisateur}
+        tableau3.append(binome3)
+        compteur3 += 1
+
+    userlist4 = User.objects.all()
+    tableau4 = []
+    compteur4 = 1
+    for utilisateur in userlist4:
+        activitylist4 = Activity.objects.filter(user=utilisateur,sport="Elliptique")
+        temps = 0
+        for activitée in activitylist4:
+            temps += activitée.durée
+        binome4 = {'temps': temps, 'utilisateur': utilisateur}
+        tableau4.append(binome4)
+        compteur4 += 1
+
+
+    userlist5 = User.objects.all()
+    tableau5 = []
+    compteur5 = 1
+    for utilisateur in userlist5:
+        activitylist5 = Activity.objects.filter(user=utilisateur,sport="Jogging")
+        temps = 0
+        for activitée in activitylist5:
+            temps += activitée.durée
+        binome5 = {'temps': temps, 'utilisateur': utilisateur}
+        tableau5.append(binome5)
+        compteur5 += 1
 
     return render(request, "leaderboard.html",locals())
 	
