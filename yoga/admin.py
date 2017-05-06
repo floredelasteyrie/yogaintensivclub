be5c007e597fdea9eb4242f06f9325371dd52712
s2l2s <stephanie@mbpdestephanie2.home>
2017-05-06 16:06:41 +0900

    Change db to PostgreSQL

--- yoga/admin.py
+++ yoga/admin.py
@@ -3,19 +3,18 @@ from .models import Activity, Sport
 
 
 class ActivityAdmin(admin.ModelAdmin):
-            list_display = ('sport', 'user', 'durée', 'date')
-            list_filter = ('user', 'sport')
-            date_hierarchy = 'date'
-            ordering = ('date', )
-            search_fields = ('user', 'sport', 'date', 'durée')
-
+    list_display = ('sport', 'user', 'durée', 'date', 'newsport')
+    list_filter = ('user', 'sport')
+    date_hierarchy = 'date'
+    ordering = ('date', )
+    search_fields = ('user', 'sport', 'date', 'durée')
 
 admin.site.register(Activity, ActivityAdmin)
 
 class SportAdmin(admin.ModelAdmin):
-            list_display = ('name', 'calories_coeff')
-            list_filter = ('name', 'calories_coeff')
-            ordering = ('name', )
-            search_fields = ('name', 'calories_coeff')
+    list_display = ('name', 'calories_coeff')
+    list_filter = ('name', 'calories_coeff')
+    ordering = ('name', )
+    search_fields = ('name', 'calories_coeff')
 
 admin.site.register(Sport, SportAdmin)
--- yoga_app/settings.py
+++ yoga_app/settings.py
@@ -88,8 +88,13 @@ WSGI_APPLICATION = 'yoga_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'icdb',
+        'USER': 'adminLasteyrie',
+        'PASSWORD': 'lasteyrie',
+        'HOST': '',
+        'PORT': 5432
+    
     }
 }
 
